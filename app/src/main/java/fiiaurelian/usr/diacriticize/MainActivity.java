67218ec771e29747bcbfc3cd0b6d016bd2505b71
Virgil Barcan <ghitza9404@gmail.com>
2016-01-30 01:28:44 +0200

    NOT FINALIZED, but enough is done in order to continue building the app! Update interface:  - Add Floating Action Button (all actions: Add diacritics, copy to clipboard will be added here)  - Used an resizable layout: when text is entered, the layout resizes itself
    Update code:
     - Eliminated creation of visual elements in code

--- app/src/main/java/fiiaurelian/usr/diacriticize/MainActivity.java
+++ app/src/main/java/fiiaurelian/usr/diacriticize/MainActivity.java
@@ -1,14 +1,12 @@
 package fiiaurelian.usr.diacriticize;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.SpannableString;
 import android.text.method.LinkMovementMethod;
-import android.text.style.ClickableSpan;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
 
@@ -24,12 +22,22 @@ public class MainActivity extends AppCompatActivity {
         dbHelper.createDatabase();
         dbHelper.openDatabase();
 
-        FrameLayout frameLayout = ( FrameLayout ) findViewById( R.id.text_container );
-        EditText inputText = new EditText( this );
-        inputText.setId(R.id.input_text);
-        inputText.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT,
-                TableRow.LayoutParams.MATCH_PARENT));
-        frameLayout.addView( inputText );
+        //ADDED IN XML
+        //LinearLayout linearLayout = ( LinearLayout ) findViewById( R.id.text_container );
+        //EditText inputText = new EditText( this );
+        //inputText.setId(R.id.input_text);
+        //inputText.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT,
+        //        TableRow.LayoutParams.MATCH_PARENT));
+        //linearLayout.addView(inputText);
+
+        FloatingActionButton floatingActionButton = (FloatingActionButton) findViewById(R.id.floating_action_button);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Click action
+                addAccents(view);
+            }
+        });
     }
 
     @Override
@@ -41,13 +49,17 @@ public class MainActivity extends AppCompatActivity {
         System.out.println("MainActivity.addAccents");
         EditText inputText = (EditText) findViewById(R.id.input_text);
         String text = inputText.getText().toString();
-        FrameLayout frameLayout = ( FrameLayout ) findViewById( R.id.text_container );
-        frameLayout.removeAllViews();
+        LinearLayout linearLayout = ( LinearLayout ) findViewById( R.id.text_container );
+        linearLayout.removeAllViews();
 
         TextView textView = new TextView(this);
         textView.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT,
                 TableRow.LayoutParams.MATCH_PARENT));
-        frameLayout.addView(textView);
+        textView.setTextSize(18); //TODO: find the default size of the EditText text
+        textView.setMaxHeight(inputText.getMaxHeight());
+        textView.setScrollContainer(true);
+        textView.setVerticalScrollBarEnabled(true);
+        linearLayout.addView(textView);
         makeWordClickable(textView, text);
     }
 
